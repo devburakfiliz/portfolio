6ca6bc7654ceb1c4e0869b7dc5d4cdac321af1ce
devburakfiliz <dev.burakfiliz@gmail.com>
2022-08-04 19:54:07 +0300

    intro add

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,6 +3,19 @@ import "./intro.scss";
 export default function Intro() {
   return (
     <div  className='intro' id="intro">
+      <div className="left">
+        <div className="imageContainer">
+          <img src="assets/mman1.png" alt=""/>
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+            <h2>Merhaba Ben</h2>
+            <h1>Burak FİLİZ</h1>
+            <h3>Freelance<span></span></h3>
+        </div>
+        
+      </div>
         
     </div>
   )
