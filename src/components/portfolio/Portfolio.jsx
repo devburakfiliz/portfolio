cd69d0005bb5a150ebaa9cb0c0f3c46448c65477
devburakfiliz <dev.burakfiliz@gmail.com>
2022-08-05 17:09:13 +0300

    porfolio page

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,6 +3,45 @@ import "./portfolio.scss";
 export default function Portfolio() {
   return (
     <div className='portfolio' id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Desing</li>
+        <li>Branding</li>
+
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="assets/mobile.png" alt=""/>
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="assets/mobile.png" alt=""/>
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="assets/mobile.png" alt=""/>
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="assets/mobile.png" alt=""/>
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="assets/mobile.png" alt=""/>
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="assets/mobile.png" alt=""/>
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="assets/mobile.png" alt=""/>
+          <h3>Banking App</h3>
+        </div>
+      </div>
 
     </div>
   )
